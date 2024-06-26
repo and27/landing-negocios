d8a7e2406186a6bb3d724fd0cc487905e7b3c482
Andrés Banda <andrezh_gln7@live.com>
2024-06-26 03:36:45 -0500

    feat: add timer banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Benefits from "@/components/Benefits";
 import Hero from "@/components/Hero";
+import TimerBanner from "@/components/TimerBanner";
 
 export default function Home() {
   return (
     <>
+      <TimerBanner />
       <Hero />
       <Benefits />
     </>
---
+++ src/components/TimerBanner.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const TimerBanner = () => {
+  const [time, setTime] = useState<string>("");
+
+  useEffect(() => {
+    const endDate = new Date("2024-07-01T19:00:00Z").getTime();
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = endDate - now;
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      setTime(`${hours} : ${minutes} : ${seconds}`);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="p-2 text-center bg-gradient-to-r from-yellow-400 to-red-500 text-semibold h-10 items-center flex justify-center">
+      <p className="text-black text-lg"> {time && `Arrancamos en: ${time}`}</p>
+    </div>
+  );
+};
+
+export default TimerBanner;
