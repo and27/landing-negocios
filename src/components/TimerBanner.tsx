5c0dcf768fad5aeeec578e2f7f66961d00191d10
Andrés Banda <andrezh_gln7@live.com>
2024-06-26 14:12:52 -0500

    feat: modify banner for ab testing

--- src/components/TimerBanner.tsx
+++ src/components/TimerBanner.tsx
@@ -5,24 +5,31 @@ const TimerBanner = () => {
   const [time, setTime] = useState<string>("");
 
   useEffect(() => {
-    const endDate = new Date("2024-07-01T19:00:00Z").getTime();
-    const interval = setInterval(() => {
-      const now = new Date().getTime();
-      const distance = endDate - now;
-      const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      setTime(`${hours} : ${minutes} : ${seconds}`);
-    }, 1000);
-
-    return () => clearInterval(interval);
+    // const endDate = new Date("2024-07-01T19:00:00Z").getTime();
+    // const interval = setInterval(() => {
+    //   const now = new Date().getTime();
+    //   const distance = endDate - now;
+    //   const hours = Math.floor(
+    //     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    //   )
+    //     .toString()
+    //     .padStart(2, "0");
+    //   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    //     .toString()
+    //     .padStart(2, "0");
+    //   const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+    //     .toString()
+    //     .padStart(2, "0");
+    //   setTime(`${hours}:${minutes}:${seconds}`);
+    // }, 1000);
+    // return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="p-2 text-center bg-gradient-to-r from-yellow-400 to-red-500 text-semibold h-10 items-center flex justify-center">
-      <p className="text-black text-lg"> {time && `Arrancamos en: ${time}`}</p>
+      <p className="text-black text-sm md:text-lg">
+        ¡Aprovecha el descuento especial!. Quedan 11 descuentos disponibles.{" "}
+      </p>
     </div>
   );
 };
