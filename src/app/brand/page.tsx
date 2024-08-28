1503f41e720febaafd71e9bdbf85c9ae8b1730e2
Andrés Banda <andrezh_gln7@live.com>
2024-08-28 18:39:30 -0500

    refactor: separate components for brand landing

--- src/app/brand/page.tsx
+++ src/app/brand/page.tsx
@@ -1,83 +1,25 @@
+"use client";
+import { useState } from "react";
 import VideoComponent from "@/components/Video";
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import BrandHero from "@/components/BrandHero";
+import BrandCTA from "@/components/BrandCTA";
+import BrandHeader from "@/components/BrandHeader";
 
 const Brand5DaysPage: React.FC = () => {
   const [videoType, setVideoType] = useState<string | null>(null);
 
-  useEffect(() => {
-    const handleWhatsAppClick = () => {
-      if (window.gtag && videoType) {
-        window.gtag("event", "click", {
-          event_category: "Conversion",
-          event_label: `WhatsApp Signup - ${videoType}`,
-          value: 1,
-        });
-      }
-    };
-
-    const whatsappLinks = document.querySelectorAll('a[href*="wa.me"]');
-    whatsappLinks.forEach((link) => {
-      link.addEventListener("click", handleWhatsAppClick);
-    });
-
-    return () => {
-      whatsappLinks.forEach((link) => {
-        link.removeEventListener("click", handleWhatsAppClick);
-      });
-    };
-  }, [videoType]);
-
   return (
-    <>
-      <section
-        className="bg-emerald-800 text-neutral-100 py-20 px-3"
-        style={{ background: "#000D14" }}
-      >
-        <div className="fixed top-0 left-0 right-0 bg-gradient-to-r from-yellow-400 to-orange-500 text-semibold text-black text-center py-3 z-50 px-2">
-          ¡Última oportunidad! El acceso gratuito a nuestro reto termina hoy.{" "}
-          <a
-            className="underline font-bold"
-            href="https://wa.me/+593997019475?text=Hola%2C%20quiero%20unirme%20al%20reto%20de%20los%205%20d%C3%ADas"
-          >
-            ¡Inscríbete ya!
-          </a>
-        </div>
-
-        <div className="container mx-auto mt-10 md:mt-20 max-w-4xl flex flex-col items-center">
-          <div className="text-center mb-6">
-            <Image
-              src="/logo.png"
-              alt="Brand 5 Days"
-              width={50}
-              height={50}
-              className="rounded-lg md:w-16 md:h-16 mx-auto"
-            />
-          </div>
-          <h1 className="text-4xl md:text-6xl font-semibold text-center mx-auto tracking-tight mb-5 md:mb-10">
-            <span className="bg-neutral-800/50 px-5 py-1 rounded-md text-3xl md:text-5xl leading-snug">
-              Transforma tu Marca en 5 Días
-            </span>
-            <br /> Crea Contenido Irresistible que Conecte y Venda
-          </h1>
-          <p className="text-center mt-5 md:text-xl max-w-3xl mx-auto">
-            Únete a nuestro reto y descubre cómo aumentar la interacción y
-            ventas de tu marca en solo 5 días.
-          </p>
-          <div className="flex justify-center mt-10">
-            <Link
-              href="https://wa.me/+593997019475?text=Hola%2C%20quiero%20unirme%20al%20reto%20de%20los%205%20d%C3%ADas"
-              className="bg-green-500 text-neutral-900 font-semibold py-3 px-8 
-              rounded-md text-xl hover:bg-green-600 transition-transform hover:scale-110 transform duration-300 ease-in-out"
-            >
-              {`QUIERO UNIRME >>`}
-            </Link>
-          </div>
-          <VideoComponent setVideoType={setVideoType} />
-        </div>
-      </section>
-    </>
+    <section
+      className="bg-emerald-800 text-neutral-100 px-3"
+      style={{ background: "#000D14" }}
+    >
+      <div className="container mx-auto mt-16 max-w-4xl flex flex-col items-center">
+        <BrandHeader />
+        <BrandHero />
+        <BrandCTA videoType={videoType} />
+        <VideoComponent setVideoType={setVideoType} />
+      </div>
+    </section>
   );
 };
 
